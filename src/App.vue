ce3fe11a26fda915ee072495d448cd8f270602c8
Flandre <flantan233@gmail.com>
2019-03-13 15:30:46 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
             <v-icon>dashboard</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
+            <v-list-tile-title>Dashboard🐴</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="">
