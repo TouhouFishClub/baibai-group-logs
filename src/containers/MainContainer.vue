beb2e61f2ce6a07ec0294acf37295b5df0cea245
Flandre <flantan233@gmail.com>
2019-03-20 15:21:08 +0800

    change href

--- src/containers/MainContainer.vue
+++ src/containers/MainContainer.vue
@@ -5,7 +5,7 @@
         <v-flex shrink>
           <v-tooltip right>
             <template v-slot:activator="{ on }">
-              <v-btn :href="source" icon large target="_blank" v-on="on">
+              <v-btn href="" icon large target="_blank" v-on="on">
                 <v-icon large>code</v-icon>
               </v-btn>
             </template>
