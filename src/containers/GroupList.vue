c6688cc58f8f56bd41e793871d75cc5dfb640c21
Flandre <flantan233@gmail.com>
2019-03-20 16:44:09 +0800

    update group

--- src/containers/GroupList.vue
+++ src/containers/GroupList.vue
@@ -6,19 +6,19 @@
     app
   >
     <v-list two-line>
-      <template v-for="(item, index) in items">
+      <template v-for="(group, index) in groupInfo">
         <v-list-tile
-          :key="item.title"
+          :key="index"
           avatar
           @click=""
         >
           <v-list-tile-avatar>
-            <img :src="item.avatar">
+            <img :src="group.avatar_url">
           </v-list-tile-avatar>
 
           <v-list-tile-content>
-            <v-list-tile-title v-html="item.title"></v-list-tile-title>
-            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+            <v-list-tile-title v-html="group.group_name"></v-list-tile-title>
+            <v-list-tile-sub-title v-html="group.group_id"></v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-divider inset ></v-divider>
@@ -39,11 +39,25 @@
             title: 'Brunch this weekend?',
             subtitle: "aaaaaaa"
           }
+        ],
+        groupInfo: [
+
         ]
       }
     },
     mounted() {
-
+      this.$axios.get('http://flanb.msharebox.com:10086/get_group_list')
+        .then(response => {
+          let res = response.data
+          if(res.status === 'ok'){
+            this.groupInfo = res.data.map(d => Object.assign({avatar_url: `http://p.qlogo.cn/gh/${d.group_id}/${d.group_id}/100`}, d))
+          } else {
+            console.log('请求失败')
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 </script>
