1c76335e8e7c67000d4bce92497522a51c7a0c28
Flandre <flantan233@gmail.com>
2019-03-28 18:07:36 +0800

    update

--- src/containers/MainContainer.vue
+++ src/containers/MainContainer.vue
@@ -1,29 +1,46 @@
 <template>
   <v-content>
-    <v-container fluid fill-height>
-      <v-layout justify-center align-center>
-        <v-flex shrink>
-          {{$store.state.actionGroupId}}
-          <v-tooltip right>
-            <template v-slot:activator="{ on }">
-              <v-btn href="" icon large target="_blank" v-on="on">
-                <v-icon large>code</v-icon>
-              </v-btn>
-            </template>
-            <span>Source</span>
-          </v-tooltip>
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <v-layout align-space-between justify-space-between column fill-height>
+      <v-flex>
+        bbb
+      </v-flex>
+      <v-flex flex-content-height>
+        aaa
+      </v-flex>
+    </v-layout>
+    <!--<v-container fluid fill-height>-->
+      <!--<v-layout row>-->
+        <!--aaa-->
+      <!--</v-layout>-->
+      <!--<v-layout row justify-center align-center>-->
+        <!--<v-flex shrink>-->
+          <!--{{$store.state.actionGroupId}}-->
+          <!--<v-tooltip right>-->
+            <!--<template v-slot:activator="{ on }">-->
+              <!--<v-btn href="" icon large target="_blank" v-on="on">-->
+                <!--<v-icon large>code</v-icon>-->
+              <!--</v-btn>-->
+            <!--</template>-->
+            <!--<span>Source</span>-->
+          <!--</v-tooltip>-->
+        <!--</v-flex>-->
+      <!--</v-layout>-->
+    <!--</v-container>-->
   </v-content>
 </template>
 
 <script>
   export default {
-    name: "main-container"
+    name: "main-container",
   }
 </script>
 
 <style scoped>
+  .flex-content-height {
+    max-height: 180px;
+    min-height: 100px;
+    border-top: 1px solid darkgrey;
+    padding: 15px;
+  }
 
 </style>
