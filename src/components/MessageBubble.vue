352594c13ef64d5ee96e9e1bd00c1d8e790febf1
Flandre <flantan233@gmail.com>
2019-04-11 17:57:26 +0800

    change bubble theme

--- src/components/MessageBubble.vue
+++ src/components/MessageBubble.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <p>{{userId}}</p>
-    <p>{{nick}}</p>
-    <p>{{timestamp}}</p>
-    <p>{{message}}</p>
-    <img :src="avatar" alt="">
-    <hr>
+  <div class="message-item">
+    <img :src="avatar" class="user-avatar">
+    <div class="message-info">
+      <span class="user-nick">{{nick}}</span>
+      <span class="msg-time">{{timestamp}}</span>
+    </div>
+    <div class="msg-text">
+      {{message}}
+    </div>
   </div>
 </template>
 
@@ -32,6 +34,49 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .message-item {
+    margin-bottom: 20px;
+    padding-left: 55px;
+    position: relative;
+    .user-avatar {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+    }
+    .message-info {
+      font-size: 14px;
+      .msg-time {
+        color: #afafaf;
+        font-size: 12px;
+      }
+    }
+    .msg-text {
+      margin-top: 10px;
+      padding: 10px;
+      background: rgba(155, 155, 155, 0.2);
+      display: inline-block;
+      word-break: break-all;
+      border-radius: 10px;
+      position: relative;
+      &:after {
+        content: '';
+        height: 0;
+        width: 0;
+        border: 6px solid rgba(155, 155, 155, 0.2);
+        border-bottom-color: transparent;
+        border-left-color: transparent;
+        position: absolute;
+        left: -12px;
+        top: 10px;
+      }
+      .msg-image {
+        max-width: 100%;
+      }
+    }
+  }
 
 </style>
