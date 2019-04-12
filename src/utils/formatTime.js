d6a6f2f50704593bab24f574bb50e9668e846379
yayoiqqq <178530237@qq.com>
2019-04-12 16:44:35 +0800

    调了下代码的格式嗯......

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,5 @@
-function addZero(data) {
-  return data < 10 ? "0" + data : data;
-}
+const addZero = data => data < 10 ? "0" + data : data;
 export default timestamp => {
-  let time = new Date(timestamp)
-  return `${time.getFullYear()}-${addZero(time.getMonth() + 1)}-${addZero(time.getDate())} ${addZero(time.getHours())}:${addZero(time.getMinutes())}:${addZero(time.getSeconds())}`
-}
+  let time = new Date(timestamp);
+  return `${time.getFullYear()}-${addZero(time.getMonth() + 1)}-${addZero(time.getDate())} ${addZero(time.getHours())}:${addZero(time.getMinutes())}:${addZero(time.getSeconds())}`;
+};
