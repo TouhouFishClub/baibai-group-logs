f10c6e80a440ffe0603534949030e34dcd71a884
yayoiqqq <178530237@qq.com>
2019-04-12 15:07:47 +0800

    修复了一个切换群滚动条不归位的问题

--- src/containers/MainMessage.vue
+++ src/containers/MainMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid fill-height test-con>
+  <v-container fluid fill-height test-con ref="messageBox">
     <v-layout column>
       <template v-for="message in messageData">
         <MessageBubble
@@ -8,51 +8,61 @@
           :timestamp="message.ts"
           :message="message.d"
           :avatar="message.avatar"
-        >
-        </MessageBubble>
+        ></MessageBubble>
       </template>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  import MessageBubble from '@/components/MessageBubble'
-  export default {
-    name: "main-message",
-    data() {
-      return {
-        nowGroupId: this.$store.getters.nowGroupId,
-        messageData:[]
-      }
-    },
-    components: {
-      MessageBubble
-    },
-    computed: {
-      getGroupId() {
-        return this.$store.getters.nowGroupId
-      }
-    },
-    watch: {
-      getGroupId(val, oldVal) {
-        this.$axios.get(`http://flanb.msharebox.com:10086/chathistory?gid=${this.$store.getters.nowGroupId}&ts=`)
-          .then(res => {
-            this.messageData = res.data.d.map(msg => Object.assign({
-              avatar: `http://q1.qlogo.cn/g?b=qq&nk=${msg.uid}&s=100`
-            }, msg))
-          })
-          .catch(err => {
-            console.log(err)
-          })
-        this.nowGroupId = this.$store.getters.nowGroupId
-      }
+import MessageBubble from "@/components/MessageBubble";
+export default {
+  name: "main-message",
+  data() {
+    return {
+      nowGroupId: this.$store.getters.nowGroupId,
+      messageData: []
+    };
+  },
+  components: {
+    MessageBubble
+  },
+  computed: {
+    getGroupId() {
+      return this.$store.getters.nowGroupId;
+    }
+  },
+  watch: {
+    getGroupId(val, oldVal) {
+      this.$axios
+        .get(
+          `http://flanb.msharebox.com:10086/chathistory?gid=${
+            this.$store.getters.nowGroupId
+          }&ts=`
+        )
+        .then(res => {
+          this.$refs.messageBox.scrollTop = 0;
+          this.messageData = res.data.d.map(msg =>
+            Object.assign(
+              {
+                avatar: `http://q1.qlogo.cn/g?b=qq&nk=${msg.uid}&s=100`
+              },
+              msg
+            )
+          );
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      this.nowGroupId = this.$store.getters.nowGroupId;
     }
   }
+};
 </script>
 
 <style scoped>
-  .test-con {
-    overflow-y: auto;
-    max-height: 100%;
-  }
+.test-con {
+  overflow-y: auto;
+  max-height: 100%;
+}
 </style>
