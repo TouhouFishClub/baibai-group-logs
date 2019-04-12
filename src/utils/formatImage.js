6c168f7626dc21f9e1c69710e8d9813a69acbf5d
yayoiqqq <178530237@qq.com>
2019-04-12 15:02:57 +0800

    忘了去掉一个控制台打印....

--- src/utils/formatImage.js
+++ src/utils/formatImage.js
@@ -2,7 +2,6 @@ export default message => {
     return message.replace(/\[CQ:image.+?\]/g, substr => {
         let newSubStr = substr.split("url=")[1];
         if (newSubStr) {
-            console.log(`http://flanb.msharebox.com:10086/image?url=${newSubStr.slice(0, -1)}`);
             return `<img class="msg-image" src="http://flanb.msharebox.com:10086/image?url=${newSubStr.slice(0, -1)}">`;
         }
         newSubStr = substr.split("file=")[1].slice(0, -1);
