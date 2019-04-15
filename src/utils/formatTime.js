45b37f92d4cdcb19a4958757026b785fa265d599
Flandre <flantan233@gmail.com>
2019-04-15 09:52:17 +0800

    change utils type

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,5 +1,7 @@
-const addZero = data => data < 10 ? "0" + data : data;
-export default timestamp => {
-  let time = new Date(timestamp);
-  return `${time.getFullYear()}-${addZero(time.getMonth() + 1)}-${addZero(time.getDate())} ${addZero(time.getHours())}:${addZero(time.getMinutes())}:${addZero(time.getSeconds())}`;
+export function addZero(data){
+  return data < 10 ? "0" + data : data
+}
+export default function formatTime(timestamp) {
+  let time = new Date(timestamp)
+  return `${time.getFullYear()}-${addZero(time.getMonth() + 1)}-${addZero(time.getDate())} ${addZero(time.getHours())}:${addZero(time.getMinutes())}:${addZero(time.getSeconds())}`
 };
