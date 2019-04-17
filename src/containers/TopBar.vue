baee6b05115d275a9629b65e078fba266cdfed6b
Flandre <flantan233@gmail.com>
2019-04-17 14:36:49 +0800

    change top button type

--- src/containers/TopBar.vue
+++ src/containers/TopBar.vue
@@ -6,7 +6,7 @@
     <v-spacer></v-spacer>
     <v-tooltip left>
       <template v-slot:activator="{ on }">
-        <v-btn icon large target="_blank" v-on="on" @click="$store.commit('updateMsg', Date.now())">
+        <v-btn icon target="_blank" v-on="on" @click="$store.commit('updateMsg', Date.now())">
           <v-icon>refresh</v-icon>
         </v-btn>
       </template>
@@ -14,7 +14,7 @@
     </v-tooltip>
     <v-tooltip left>
       <template v-slot:activator="{ on }">
-        <v-btn icon large target="_blank" v-on="on" @click="changeDarkTheme">
+        <v-btn icon target="_blank" v-on="on" @click="changeDarkTheme">
           <v-icon>{{$store.state.isDark ? 'wb_sunny' : 'brightness_2'}}</v-icon>
         </v-btn>
       </template>
