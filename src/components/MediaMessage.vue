eeebff825a0aac0dda2221b649fe1bf1328d2016
Flandre <flantan233@gmail.com>
2019-04-18 11:50:11 +0800

    update share type

--- src/components/MediaMessage.vue
+++ src/components/MediaMessage.vue
@@ -6,6 +6,13 @@
         <img class="msg-image" v-if="m.url" :src="`http://flanb.msharebox.com:10086/image?url=${m.url}`">
         <img class="msg-image" v-else :src="`http://flanb.msharebox.com:10086/image?d=${m.file}`">
       </template>
+      <div v-else-if="m.cqType == 'share'" class="msg-share" @click="openUrl(m.url)">
+        <p class="share-title">{{m.title}}</p>
+        <div class="share-desc-container">
+          <div class="content">{{m.content}}</div>
+          <img :src="m.image" class="share-img">
+        </div>
+      </div>
       <span v-else>{{JSON.stringify(m)}}</span>
     </template>
     <!--<p>{{JSON.stringify(msgData)}}</p>-->
@@ -23,6 +30,9 @@
     methods: {
       formatText(msg) {
         return msg.replace(/\n/g, '<br>')
+      },
+      openUrl(url){
+        window.open(url)
       }
     }
   }
@@ -55,6 +65,40 @@
     .msg-image {
       max-width: 100%;
       vertical-align:bottom;
+    }
+    .msg-share {
+      max-width: 220px;
+      p.share-title {
+        font-size: 18px;
+        line-height: 22px;
+        font-weight: bold;
+        margin-bottom: 4px;
+        max-height: 44px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        flex-direction: column;
+      }
+      .share-desc-container {
+        display: flex;
+        cursor: pointer;
+        .content {
+          line-height: 20px;
+          max-height: 60px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 3;
+          flex-direction: column;
+        }
+        .share-img {
+          width: 60px;
+          height: 60px;
+          margin-left: 4px;
+          flex-shrink: 0;
+        }
+      }
     }
   }
 </style>
