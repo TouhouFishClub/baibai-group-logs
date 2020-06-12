3bd4a03b2191f8e688210641817593973a0b3c7c
flandre <flantan233@gmail.com>
2020-06-12 17:30:36 +0800

    change setting

--- src/main.js
+++ src/main.js
@@ -11,6 +11,53 @@ import Viewer from 'v-viewer'
 Vue.config.productionTip = false
 Vue.use(Viewer)
 
+Viewer.setDefaults({
+  navbar: false,
+  toolbar: {
+    zoomIn: {
+      show: 1,
+      size: 'large',
+    },
+    zoomOut: {
+      show: 1,
+      size: 'large',
+    },
+    oneToOne: {
+      show: 1,
+      size: 'large',
+    },
+    reset: {
+      show: 1,
+      size: 'large',
+    },
+    prev: 0,
+    play: {
+      show: 1,
+      size: 'large',
+      click(...args) {
+        console.log(args)
+      }
+    },
+    next: 0,
+    rotateLeft: {
+      show: 1,
+      size: 'large',
+    },
+    rotateRight: {
+      show: 1,
+      size: 'large',
+    },
+    flipHorizontal: {
+      show: 1,
+      size: 'large',
+    },
+    flipVertical: {
+      show: 1,
+      size: 'large',
+    },
+  },
+})
+
 new Vue({
   router,
   store,
